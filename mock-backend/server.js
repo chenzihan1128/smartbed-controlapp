554bfc78497d53e10b9740b94a1f525f299826c9
Zihan Chen <chenzihan1128@gmail.com>
2026-03-23 04:13:28 +0800

    Allow cross-origin frontend requests

--- mock-backend/server.js
+++ mock-backend/server.js
@@ -7,6 +7,19 @@ import { initBedController, shutdownBedController } from "./services/bedService.
 import { sendAlertEmail, sendTestEmail } from "./services/emailService.js";
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(express.json());
 
 function rand(base, range) {
